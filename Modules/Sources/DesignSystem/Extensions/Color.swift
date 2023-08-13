d2dd356c59d76547ee5cb7b08d9d69fde9a98d70
arman <arman.arutiunov@gmail.com>
2023-08-13 17:07:52 +0100

    ✨ Create Colors asset catalog + add Cherry color

---
+++ Modules/Sources/DesignSystem/Extensions/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  
+//
+//  Created by Arman Arutiunov on 13/08/2023.
+//
+
+import SwiftUI
+
+extension Color {
+
+    public static let cherry = Color("Cherry", bundle: .designSystem)
+}
