531a0c60233d0c8ab11b16a027fa5c6faace496f
arman <arman.arutiunov@gmail.com>
2023-08-13 22:11:25 +0100

    ✨ RoomJSON

---
+++ Modules/Tests/RoomManagerTests/JSONs.swift
@@ -0,0 +1,15 @@
+//
+//  JSONs.swift
+//  
+//
+//  Created by Arman Arutiunov on 13/08/2023.
+//
+
+@testable import RoomManager
+import Foundation
+
+let RoomJSON: [String: Any] = [
+    Room.CodingKeys.name.rawValue: "Ljerka",
+    Room.CodingKeys.spotsCount.rawValue: 43,
+    Room.CodingKeys.imageURL.rawValue: "https://image.com"
+]
