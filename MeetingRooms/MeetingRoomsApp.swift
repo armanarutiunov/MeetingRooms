cc81b486df68774bbcc33f60b8ee59e1376e7220
arman <arman.arutiunov@gmail.com>
2023-08-13 14:31:12 +0100

    Initial commit

---
+++ MeetingRooms/MeetingRoomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingRoomsApp.swift
+//  MeetingRooms
+//
+//  Created by Arman Arutiunov on 13/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MeetingRoomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
